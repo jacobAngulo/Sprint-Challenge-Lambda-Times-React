60b75b87dd4cac61e3749ace91b427f30b8663af
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-22 09:49:04 -0800

    mvp

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -35,19 +35,57 @@ flex: 1;
 font-size: 11px;
 `
 
+const TopBarContainerCenterStylez = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: row;
+flex: 3;
+font-size: 9px;
+`
+
+const TopBarContainerRightStylez = styled.div`
+display: flex;
+justify-content: flex-end;
+align-items: center;
+flex-direction: row;
+flex: 1;
+font-size: 11px;
+font-weight: bold;
+`
+
+const LeftSpan = styled.span`
+cursor: pointer;
+margin-right: 25%;
+font-weight: bold;
+`
+
+const CenterSpan = styled.span`
+cursor: pointer;
+margin-right: 5%;
+`
+
+const LastCenterSpan = styled.span`
+cursor: pointer;
+margin-right: 0;
+`
+const RightSpan = styled.span`
+cursor: pointer;
+`
+
 const TopBar = () => {
   return (
     <TopBarStylez>
       <TopBarContainerStylez>
         <TopBarContainerLeftStylez>
-          <span>TOPICS</span><span>SEARCH</span>
+          <LeftSpan>TOPICS</LeftSpan><LeftSpan>SEARCH</LeftSpan>
         </TopBarContainerLeftStylez>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
+        <TopBarContainerCenterStylez>
+          <CenterSpan>GENERAL</CenterSpan><CenterSpan>BROWNBAG</CenterSpan><CenterSpan>RANDOM</CenterSpan><CenterSpan>MUSIC</CenterSpan><LastCenterSpan>ANNOUNCEMENTS</LastCenterSpan>
+        </TopBarContainerCenterStylez>
+        <TopBarContainerRightStylez>
+          <RightSpan>LOG IN</RightSpan>
+        </TopBarContainerRightStylez>
       </TopBarContainerStylez>
     </TopBarStylez>
   )
