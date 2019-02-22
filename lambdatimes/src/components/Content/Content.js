4cb415a83133a7673818832f3b7dcb731bec9eeb
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-22 08:49:04 -0800

    selected tab has a unique class

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,10 +17,14 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
+    this.setState({
+      tabs: tabData,
+      cards: cardData
+    })
     // Once the component has mounted, get the data and reflect that data on the state.
   }
 
-  changeSelected = tab => {
+  changeSelected = tab => { 
     // this function should take in the tab and update the state with the new tab.
   };
 
@@ -48,7 +52,9 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+        selectedTab={this.state.selected}
+        tabs={this.state.tabs} />
         <Cards cards={this.filterCards()} />
       </div>
     );
